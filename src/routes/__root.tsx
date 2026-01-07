4912ba98fe0d457bab8849279dd6e3dd125185d6
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-07 09:29:12 -0500

    chore: add favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,6 +28,10 @@ export const Route = createRootRoute({
         rel: 'stylesheet',
         href: appCss,
       },
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
     ],
   }),
 
