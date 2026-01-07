cbe448f21f44429feb7898e3bd8b3b05453b57e4
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-07 11:02:36 -0500

    fix: move DialogHeader inside DialogContent for a11y

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -43,14 +43,14 @@ function CommandDialog({
 }) {
   return (
     <Dialog {...props}>
-      <DialogHeader className="sr-only">
-        <DialogTitle>{title}</DialogTitle>
-        <DialogDescription>{description}</DialogDescription>
-      </DialogHeader>
       <DialogContent
         className={cn('overflow-hidden rounded-4xl! p-0', className)}
         showCloseButton={showCloseButton}
       >
+        <DialogHeader className="sr-only">
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
         {children}
       </DialogContent>
     </Dialog>
